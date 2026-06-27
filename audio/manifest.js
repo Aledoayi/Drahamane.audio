17b73393fcf13b991fd31510b62abd19a504d2ff
Aledoayi <92909667+Aledoayi@users.noreply.github.com>
2026-06-27 23:14:35 +0200

    Add files via upload

--- audio/manifest.js
+++ audio/manifest.js
@@ -5,6 +5,7 @@ window.AUDIO_TRACKS = [
   "LES AVENTURES DE COUMBA ET ZOULOU EN AFRIQUE DU SUD.mp3",
   "PANIQUE EN SKI NAUTIQUE.mp3",
   "Petits antômes au pays des formes et des couleurs.mp3",
+  "Salomé veut une histoire….mp3",
   "Sauvetage du Capybara.mp3",
   "Une grande demande pour Madame Poule.mp3"
 ];
